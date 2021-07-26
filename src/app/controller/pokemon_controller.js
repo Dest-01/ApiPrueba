1c0a71d7d2690b0066864bc8e224638cc83042b4
Dest-01 <cesarmatarrita484@gmail.com>
2021-07-26 10:04:33 -0600

    add proyect controller

---
+++ src/app/controller/pokemon_controller.js
@@ -0,0 +1,17 @@
+const Pokemon = require('../model');
+
+class PokemonController {
+
+   async index(req, res){
+       try{
+           const pokemon = await Pokemon.find();
+           return   res.json({
+               pokemon
+           })
+       }catch(err){
+           return res.status(404).send('error');
+       }
+   }
+}
+
+module.exports = new PokemonController;
